9af0aa56b17ef90f29a5b43373f137626e1cf9df
agilcore09 <agiljibrin009@gmail.com>
2023-11-11 23:48:07 +0800

    pertama kali

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Article from './component/Article'
+import './../public/style/index.css'
+
+function App() {
+  return <Article/>
+}
+
+export default App
---
+++ src/component/Article.jsx
@@ -0,0 +1,8 @@
+function Article(){
+    return  <div>
+                <p className="test">Ini Adalah Article</p>
+                <small>di buat oleh Agil</small>
+            </div>
+}
+
+export default Article; 
