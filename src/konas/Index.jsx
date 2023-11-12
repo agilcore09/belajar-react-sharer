e80e976c097348e1e8f68f8dd09ce6ef2a78d9c0
agilcore09 <agiljibrin009@gmail.com>
2023-11-12 22:11:35 +0800

    menambah halaman konasshare strip

--- src/konas/Index.jsx
+++ src/konas/Index.jsx
@@ -1,11 +1,13 @@
 function KonasPage(){
     return(
         <div className="body bg-slate-950 h-screen flex justify-center items-center">
-        <div className="konas-square border w-72 h-72 text-center" >
-            <img src="public/konas.png" alt="" />
-                <p className="text-white">Selamat Datang</p>
-                <h3 className="text-white font-bold">Kotak Narsis Share Strip</h3>
-                <button className="text-white bg-red-500 w-20 h-7 rounded-lg">Mulai</button>
+        <div className="konas-square w-2/5 h-3/5 text-center" >
+            <img src="public/konas.png" alt="konas" className="w-4xl mx-auto my-auto" />
+               <div className="body-content mt-10">
+                    <p className="text-white text-3xl">Selamat Datang</p>
+                    <h3 className="text-white font-bold text-5xl">Kotak Narsis Share Strip</h3>
+                    <button className="text-white bg-red-500 w-48 h-16 rounded-lg text-2xl font-bold mt-8">MULAI</button>
+               </div>
             </div>
         </div>
     )
