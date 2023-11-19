0754d4a94f204e9f5142729430ce4e84b6de01d0
unknown <agiljibrin009@gmail.com>
2023-11-19 19:01:32 +0800

    perbaikan tombol

--- src/konas/Gallery.jsx
+++ src/konas/Gallery.jsx
@@ -2,13 +2,19 @@ import data from "../images.json";
 
 function Gallery() {
     return (
-        <div className="w-full max-w-5xl p-5 pb-10 mx-auto mb-10 gap-5 columns-3 space-y-5">
-            {data.map((e) => {
-                return (
-                    <img src={e.path} alt="gambar" />
-                )
-            })}
-        </div>
+        <>
+            <div className="gallery-header my-5">
+                <h3 className="text-center text-4xl font-semibold text-red-500">Strip Gallery</h3>
+            </div>
+            <div className="w-full max-w-5xl p-5 pb-10 mx-auto mb-10 gap-5 columns-3 space-y-5">
+                {data.map((e) => {
+                    return (
+                        <img src={e.path} alt="gambar" />
+                    )
+                })}
+            </div>
+        </>
+
     )
 }
 
--- src/konas/Index.jsx
+++ src/konas/Index.jsx
@@ -1,16 +1,19 @@
-function KonasPage(){
-    return(
+import { Link } from "react-router-dom"
+
+function KonasPage() {
+    return (
         <div className="body bg-slate-950 h-screen flex justify-center items-center">
-        <div className="konas-square w-2/5 h-3/5 text-center" >
-            <img src="public/konas.png" alt="konas" className="w-4xl mx-auto my-auto" />
-               <div className="body-content mt-10">
-                    <p className="text-white text-3xl">Selamat Datang</p>
-                    <h3 className="text-white font-bold text-5xl">Kotak Narsis Share Strip</h3>
-                    <button className="text-white bg-red-500 w-48 h-16 rounded-lg text-2xl font-bold mt-8">MULAI</button>
-               </div>
+            <div className="konas-square w-2/5 h-3/5 text-center" >
+                <img src="public/konas.png" alt="konas" className="w-4xl mx-auto my-auto" />
+                <div className="body-content mt-10">
+                    <p className="text-white text-xl">Selamat Datang</p>
+                    <h3 className="text-white font-bold text-3xl">Kotak Narsis Share Strip</h3>
+                    <br />
+                    <a href="/gallery" className="text-white bg-red-500 h-35 rounded-lg text-xl px-6 py-2 font-bold mt-8 hover:py-3 hover:px-7 transition delay-150 duration-300 ease-in-out">MULAI</a>
+                </div>
             </div>
-        </div>
+        </div >
     )
-} 
+}
 
 export default KonasPage
