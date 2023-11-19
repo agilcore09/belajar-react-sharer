ef24068a18ce37a54c01cdd5355abad4feb9e4eb
unknown <agiljibrin009@gmail.com>
2023-11-19 11:16:16 +0800

    menambah gallery model

---
+++ src/konas/Gallery.jsx
@@ -0,0 +1,9 @@
+function Gallery() {
+    return (
+        <div className="w-full max-w-5xl p-5 pb-10 mx-auto mb-10 gap-5 columns-3 space-y-5">
+
+        </div>
+    )
+}
+
+export default Gallery;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './test.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import KonasPage from './konas/Index';
+import './test.css';
+import Gallery from './konas/Gallery';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <KonasPage />
+  },
+
+  {
+    path: '/gallery',
+    element: <Gallery />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
